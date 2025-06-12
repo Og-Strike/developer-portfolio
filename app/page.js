0cad57d374d9a935be87a522d88661c8d9db20ff
Nitesh Kumar Mishra <128485609+Og-Strike@users.noreply.github.com>
2025-06-12 10:40:00 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import AboutSection from './components/homepage/about';
 import Certifications from './components/homepage/certifications';
-import ContactSection from './components/homepage/contact';
 import Education from './components/homepage/education';
 import Experience from './components/homepage/experience';
 import HeroSection from './components/homepage/hero-section';
@@ -22,7 +21,6 @@ export default async function Home() {
           <Education />
           <Certifications />
           <Achievements />
-          <ContactSection />
         </>
     </div>
   );
