c03307f3c08f4fbf13c9f8e92e58cdaf3768172e
Strike <niteshmishra1702@gmail.com>
2025-06-12 13:06:35 +0530

    vb

--- utils/data/personalData.js
+++ utils/data/personalData.js
@@ -5,7 +5,7 @@ export const personalData = {
   designations: ["Software Development", "AI & ML", "Ethical Hacking", "Full Stack Development", "App Development","Database Management"],  
   description: "My name is Nitesh Kumar Mishra (Alias - Strike). I am a Passionate App Developer, Hacker, and AI & ML Enthusiast with a strong foundation in Software Development. I lead many dynamic teams in crafting innovative solutions. My expertise extends to Database Management, showcasing my ability to organize and manage large sets of data. With a knack for problem-solving and a comprehensive skill set, I thrive in creating impactful solutions that align with business objectives. Whether it's Web Development, Game Development, or AI & ML projects, I bring a versatile skill set to the table. Proficient in all popular programming languages, my logical thinking and analytical approach enable me to navigate complex challenges and deliver robust solutions.",
   email: 'niteshmishra1702@gmail.com',
-  phone: '+91 1234567890',
+  phone: '+91 9717692749',
   address: 'New Delhi, India',
   github: 'https://github.com/Og-Strike',
   instagram: 'https://www.instagram.com/itachi.strike/',
