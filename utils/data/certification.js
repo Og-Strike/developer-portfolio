7dc8b3953ab52c15914863eaa4515a0aa974454d
Nitesh Kumar Mishra <128485609+Og-Strike@users.noreply.github.com>
2026-02-16 20:11:03 +0530

    Update

--- utils/data/certification.js
+++ utils/data/certification.js
@@ -12,24 +12,6 @@ const certifications = [
     link: 'https://www.udemy.com/certificate/UC-558d6061-8b60-40b1-a3da-8f3be0d15788/'
   },
   {
-    title: 'CCNA: Introduction to Networks',
-    issuer: 'Cisco Networking Academy',
-    image: '/certi/3.png',
-    link: 'https://www.credly.com/badges/e54dc996-0f76-4f5c-9859-b35218b859fd'
-  },
-  {
-    title: 'CCNA: Switching, Routing, and Wireless Essentials',
-    issuer: 'Cisco Networking Academy',
-    image: '/certi/4.png',
-    link: 'https://www.credly.com/badges/be6b5e69-2dee-4c09-bab7-771b310445c4'
-  },
-  {
-    title: 'CCNA: Enterprise Networking, Security, and Automation',
-    issuer: 'Cisco Networking Academy',
-    image: '/certi/5.png',
-    link: 'https://www.credly.com/badges/fab5f929-8b48-4cfa-b368-45add96ed44c'
-  },
-  {
     title: 'Introduction to Data Science',
     issuer: 'Cisco Networking Academy',
     image: '/certi/6.png',
