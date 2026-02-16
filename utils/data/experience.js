aac8222954181f934655e9eef2fa9728bd33bd5d
Nitesh Kumar Mishra <128485609+Og-Strike@users.noreply.github.com>
2026-02-16 20:11:51 +0530

    Modify experience duration for Mobile App Developer Intern
    Updated the duration for the Mobile App Developer Intern position.

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,7 +3,7 @@ export const experiences = [
     id: 1,
     title: 'Mobile App Developer Intern',
     company: "MobiArmour",
-    duration: "(May 2025 - Present)",
+    duration: "(May 2025 - Aug 2025)",
     link: "https://armour.mobi/"
   },
   {
@@ -12,12 +12,5 @@ export const experiences = [
     company: "Technovate 2025 Website",
     duration: "(Feb 2025 - Mar 2025)",
     link: "https://amity.edu/aset/technovate2025/"
-  },
-  {
-    id: 3,
-    title: "Software Development",
-    company: "AKM - INNOVATORS",
-    duration: "(Jun 2022 - Jun 2025)",
-    link: "https://www.linkedin.com/company/akm-innovators"
   }
 ]
